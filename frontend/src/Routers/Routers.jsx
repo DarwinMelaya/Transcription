0b66727994874b4a79eb8d8ceafce074060c8f95
DarwinMelaya <melayadarwin@gmail.com>
2026-03-10 17:29:12 +0800

    Add LandingPage component and update routing in Routers

--- frontend/src/Routers/Routers.jsx
+++ frontend/src/Routers/Routers.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "../pages";
+import { Home, LandingPage } from "../pages";
 
 export const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </Router>
   );
